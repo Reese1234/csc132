649e5ad9f8feaa7cd65741f9c4fc863df3cf3877
AngyMaceMan <mason.lefrance@gmail.com>
2022-03-24 20:09:25 -0500

    frame displays a single button with an image

--- Project.py
+++ Project.py
@@ -1,16 +1,31 @@
 from tkinter import *
 
+#git add
+#git commit -m"comment"
+#git push
 
 
-
+# class for the screen
 class Screen(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent, bg="black")
+        self.setupGUI()
+        
+    # the setup
+    def setupGUI(self):
+        # variable for assigning the image
+        img = PhotoImage(file="C:/Users/angry/Desktop/dew.gif")
+        # the setup for the button itself, assigning the image on top of it
+        button = Button(self, bg="white", image=img)
+        button.image = img
+        # shove it in a cell
+        button.grid(row=0, column=0, columnspan=1, sticky=N + S + E + W)
+
         self.pack(fill=BOTH, expand=1)
 
 
 window = Tk()
 window.geometry("{}x{}".format(800, 800))
 
-s= Screen(window)
+s = Screen(window)
 mainloop()
--- test.py
+++ test.py
@@ -1,8 +1,38 @@
-import math
-print("Hello")
-print("this is boring")
-print("this is")
-print("git pull")
-print("Scotland is Small creature")
-print("trying new things")
-print("hello")
+#
+#
+################################
+
+from tkinter import *
+
+class Vending(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent, bg="white")
+        # call a method we write to setup GUI elements
+        self.setupGUI()
+
+    def setupGUI(self):
+        
+        img = PhotoImage(file=)
+        button = Button(self, bg="white", image=img, command=lambda:self.process("("))
+        button.image = img
+        # shove it in a cell
+        button.grid(row=0, column=0, columnspan=1, sticky=N + S + E + W)
+
+        self.pack(fill=BOTH, expand=1)
+
+
+
+
+
+
+
+### MAIN CODE ###
+# create a window
+window = Tk()
+window.title("Vending Machine")
+
+# generate the calculator object
+v = Vending(window)
+
+#display the calculator and wait for input
+window.mainloop()
