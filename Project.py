41db0bb1e25c7362d735ecd2f8c4af19a4aa20a6
Reese1234 <sealsreese25@gmail.com>
2022-04-13 19:30:31 -0500

    I improved the button system and added Comments

--- Project.py
+++ Project.py
@@ -5,13 +5,46 @@ from PIL import Image, ImageTk
 #git commit -m"comment"
 #git push
 
+# DATA_FILE_LOCATION = "C:/users/google drive/machine_data.json"
+
+# file = open(DATA_FILE_LOCATION, "r")
+
+# file = open(DATA_FILE_LOCATION, "w")
+
+# data.read()
+# data = {
+#     "machine_1": 
+#             {
+#                 "mountain": 
+#                 {
+#                     "amount": 5, 
+#                     "sold":4
+#                 },
+#                 "drpepper":
+#                 {
+#                     "amount": 5,
+#                     "sold": 4,
+#                     "price": 1.5
+#                 } 
+#             }
+#         }
+
+# data["machine_1"]["mountain"]["sold"] = 4
+
+
+# read data
+    # open json file
+    # read data
+    # close json file
+
+# write data
+    # open json file
+    # write data
+    # close json file
+    # read data
 
 # class for the screen
 class Screen(Frame):
-
-    """ framelist = [1,2]
-    frame_index = 0
-    count = 0 """
     
     
     def __init__(self, parent):
@@ -19,10 +52,9 @@ class Screen(Frame):
         self.setupGUI()
         
 
-        """ self.animate_gif(100)"""
-
     # the setup
     def setupGUI(self):
+        self.Level1()
         # BackGround in progress
         # Mason's file
         #bg = PhotoImage(file = "C:/Users/angry/Desktop/csc132/Project Buttons/La_tech.gif")
@@ -42,14 +74,8 @@ class Screen(Frame):
         self.button = Button(self, bg="white", image=img,borderwidth=0, command=lambda: self.StockPage())
         self.button.image = img
         # shove it in a cell
-        self.button.place(x=300, y=0)                                       # Change to fix RPI display
+        self.button.place(x=300, y=200)                                       # Change to fix RPI display
 
-        self.button2 = Button(self,image=img, borderwidth=0)
-        self.button2.image = img
-        # place allows you to put it in a specfic place rather than a relative column or row like grid
-        self.button2.place(x=300, y= 200)                                   # Change to fix RPI Display
-
-        self.pack(fill=BOTH, expand=1)
         # Mason's file
         #Simg = PhotoImage(file="C:/Users/angry/Desktop/csc132/Project Buttons/SettingIcon.png")
         # Reese's file
@@ -65,6 +91,10 @@ class Screen(Frame):
         self.InfoButton = Button(self, image=InfoButtonPicture, borderwidth=0,command=lambda: self.InfoPage())
         self.InfoButton.image = InfoButtonPicture
         self.InfoButton.place(x=0, y=600, relheight=0.2, relwidth=0.2)
+        Chart = PhotoImage(file="C:/Users/reese/OneDrive/Desktop/mygitfolder/Chart.png")
+        self.ChartButton = Button(self, image=Chart, borderwidth=0, command=lambda: self.ChartPage())
+        self.ChartButton.image = Chart
+        self.ChartButton.place(x=640, y=0, relheight=0.2, relwidth=0.2)
         
 
 
@@ -74,16 +104,21 @@ class Screen(Frame):
         # Clears all vending options.
         self.Level1()
         # The contents of the info menu (WIP)
-        self.label2 = Label(self, bg= "red", text="Page 2", font=("Arial", 25))
-        self.label2.place(x= 0, y=0, relheight=1, relwidth=1)
+        self.label.pack(fill=BOTH, expand=1)
         self.BackButton = Button(self, text="Click here to go back", bg="white", command=lambda: self.setupGUI() )
         self.BackButton.place(x=350, y =100)
         self.pack(fill=BOTH, expand=1)
 
+    # Controls the Chart page after you click the chart button
+    def ChartPage(self):
+        # Cleans the Board
+        self.Level1()
+        # packs the background image
+        self.label.pack(fill=BOTH, expand=1)
+        # Re intializes the Back Button
+        self.BackButton = Button(self, text = "Go Back", command= lambda: self.setupGUI())
+        self.BackButton.place(x= 100, y=100)
 
-        
-        #Label(self, bg="cyan", text="Hey")
-        #Label.pack( fill=BOTH, expand=1)
     
     # This function creates the settings page. 
     # It clears the screen of all vending options and displays the options menu.
@@ -92,40 +127,41 @@ class Screen(Frame):
         # Clears all vending options
         self.Level1()
         # The contents of the options menu (WIP)
-        self.SettingsLabel = Label(self, bg="white", text="This will be the settings page where we have options and stuff", font=("Arial", 13), padx=-30)
-        self.SettingsLabel.place(x=0, y=0, relheight=1, relwidth=1)
-        self.BackButton2 = Button(self, text="Click here to go back", bg="white", command=lambda: self.setupGUI() )
-        self.BackButton2.place(x=350, y =100)
+        self.label.pack(fill=BOTH, expand=1)
+        self.BackButton = Button(self, text="Click here to go back", bg="white", command=lambda: self.setupGUI() )
+        self.BackButton.place(x=350, y =100)
         self.pack(fill=BOTH, expand=1)
         
     def StockPage(self):
         # Clears all vending options
         self.Level1()
-        self.StockLabel = Label(self, bg="white")
-        self.StockLabel.place(x=0, y=0, relheight=1, relwidth=1)
+        self.label.pack(fill=BOTH, expand=1)
         self.Mountaindew()
 
+    # Clean up Function, Wipes the Screen
     def Level1(self):
-        self.SettingIcon.destroy()
-        self.InfoButton.destroy()
-        self.button.destroy()
-        self.button2.destroy()
-        self.label.destroy()
         try:
-            self.label2.destroy()
+            self.SettingIcon.destroy()
+            self.InfoButton.destroy()
+            self.button.destroy()
+            self.label.forget()
+        except:
+            pass
+        try:
             self.BackButton.destroy()
         except:
             pass
         try:
-            self.SettingsLabel.destroy()
-            self.BackButton2.destroy()
+            self.ChartButton.destroy()
         except:
             pass
         try:
-            self.StockLabel.destroy()
-            self.BackButton4.destroy()
+            self.label5.destroy()
         except:
             pass
+
+
+    # Controls the Animation on the Stock page of the Mountain Dew
     def Mountaindew(self):
         img = Image.open(r"C:/Users/reese/OneDrive/Desktop/mygitfolder/dew.gif")
         framesTotal = img.n_frames
@@ -151,41 +187,14 @@ class Screen(Frame):
             self.after(play_back_delay, update, ind)
         # the setup for the button itself, assigning the image on top of it
         self.label5 = Label(self)
-        self.label5.pack(fill=BOTH, expand=2, anchor=N)
+        self.label5.place(x=350, y = 300)
         loadGif()
         update(0)
-        self.BackButton4 = Button(self, text="click here")
-        self.BackButton4.place(x=250, y=100)
+        self.BackButton = Button(self, text="click here", command= lambda: self.setupGUI())
+        self.BackButton.place(x=250, y=100)
 
 
-    """ def animate_gif(self, count):
-        print(self.framelist)
-        Label.config(image = self.framelist[count])
-        print(self.framelist)
-        count +=1
-        if count > 10:
-            count = 0
-        self.pack(fill=BOTH, expand=1)
-        
-        self.after(100, lambda : self.animate_gif(count))
-
-    def run_animation(self):
-        while True:
-            try:
-                bg = PhotoImage(file = "C:/Users/reese/OneDrive/Desktop/mygitfolder/La_tech.gif", format = "gif - {}".format(frame))
-                
-
-                frame = frame + 1
-
-            except:
-                print("break")
-                last_frame = self.frame_index - 1
-                break
-            self.framelist.append(frame)
-            print(len(self.framelist), "frame")
-            self.frame_index += 1
- """
-
+############################# Main Function ########################################################################################################
 window = Tk()
 window.geometry("{}x{}".format(800, 800))
 s = Screen(window)
